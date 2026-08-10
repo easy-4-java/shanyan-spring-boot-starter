00950e9aa571f8083ce7aed93c14acef99425e4f
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:06:21 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/com/shanyan/spring/boot/FlashMobileProperties.java
+++ src/main/java/com/shanyan/spring/boot/FlashMobileProperties.java
@@ -21,7 +21,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @ConfigurationProperties(FlashMobileProperties.PREFIX)
 public class FlashMobileProperties {
